3fca0895323bb10debf5ab2fd5f797c72e9f8611
Steffen Kroggel <kroggel@rkw.de>
2023-03-24 15:17:52 +0100

    [FIX] minor fixes and improvements

--- Classes/Service/Geolocation.php
+++ Classes/Service/Geolocation.php
@@ -1,9 +1,20 @@
 <?php
-
 namespace RKW\RkwGeolocation\Service;
 
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
 use Madj2k\CoreExtended\Utility\GeneralUtility;
-use Madj2k\CoreExtended\Utility\GeneralUtility as Common;
 use TYPO3\CMS\Core\Log\Logger;
 use TYPO3\CMS\Core\Log\LogManager;
 use \TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
@@ -415,7 +426,7 @@ class Geolocation implements \TYPO3\CMS\Core\SingletonInterface
      */
     protected function getSettings(string $which = ConfigurationManagerInterface::CONFIGURATION_TYPE_SETTINGS): array
     {
-        return Common::getTypoScriptConfiguration('RkwGeolocation', $which);
+        return GeneralUtility::getTypoScriptConfiguration('RkwGeolocation', $which);
     }
 
 
